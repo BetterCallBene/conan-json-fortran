d1cd7ec3e96aacadebf2f08eb4a58b54d622da14
Benedikt König <benedikt.koenig@tum.de>
2018-04-13 22:08:10 +0200

    Initial Commit

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, CMake, tools
+
+
+class JsonfortranConan(ConanFile):
+    name = "json-fortran"
+    version = "6.2.0"
+    license = "https://github.com/jacobwilliams/json-fortran/blob/master/LICENSE"
+    url = "https://github.com/jacobwilliams/json-fortran"
+    description = "A Fortran 2008 JSON API"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    exports_sources = ["src/*"]
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+        cmake.install()
+
+        # Explicit way:
+        # self.run('cmake %s/hello %s'
+        #          % (self.source_folder, cmake.command_line))
+        # self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        pass
+
+    def package_info(self):
+        self.cpp_info.libs = [self.name]
+
